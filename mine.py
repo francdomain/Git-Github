de7b86f45ff684487945ef449b99954fca98d994
francdomain <cfrancmail@gmail.com>
2023-10-16 06:23:32 +0100

    Updated the name and email

--- mine.py
+++ mine.py
@@ -1,6 +1,6 @@
 # Define variables
-name = "PUT YOUR NAME"
-email = 'PUT YOUR EMAIL'
+name = " FRANCIS CHUKWUOMA"
+email = 'CFRANCMAIL@GMAIL.COM'
 
 # Print the name and email address
 print("Name:", name)
